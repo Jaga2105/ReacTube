f615c8e1221f9980ccb7c872706f7910c97139e3
Jagannath Nayak <jagannathnayak2105@gmail.com>
2023-08-05 01:14:08 +0530

    minor changes

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,7 +2,6 @@ import React from "react";
 import VideoContainer from "./VideoContainer";
 import Carousell from "./Carousell";
 
-
 const MainContainer = () => {
   return (
     <div className="">
